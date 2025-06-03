cd4b95a4317d6a26e58645a0e79f0952133a7699
claudio <claudio.datos@gmail.com>
2025-06-03 01:09:22 -0400

    cuarto commit

--- app/Http/Controllers/BeneficioController.php
+++ app/Http/Controllers/BeneficioController.php
@@ -21,7 +21,7 @@ class BeneficioController extends Controller
     {
         $responseFiltros = Http::get('https://run.mocky.io/v3/b0ddc735-cfc9-410e-9365-137e04e33fcf');
         $responseFichas = Http::get('https://run.mocky.io/v3/4654cafa-58d8-4846-9256-79841b29a687');
-        $responseBeneficios = Http::get('https://run.mocky.io/v3/8f75c4b5-ad90-49bb-bc52-f1fc0b4aad02'); // Cambiar por el real
+        $responseBeneficios = Http::get('https://run.mocky.io/v3/8f75c4b5-ad90-49bb-bc52-f1fc0b4aad02'); 
 
         if (!$responseFiltros->successful() || !$responseFichas->successful() || !$responseBeneficios->successful()) {
             return response()->json([
